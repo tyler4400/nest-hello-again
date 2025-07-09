8db5bb005e0c2e709bd6037e0c2628c4e5e272b0
tyler4400 <tyler4400@foxmail.com>
2025-07-09 17:43:33 +0800

    日志学习 之 nestjs-pino

--- src/log/log.controller.ts
+++ src/log/log.controller.ts
@@ -1,15 +1,25 @@
 import { Controller, Logger, Get } from '@nestjs/common';
 import { LogService } from './log.service';
+import { PinoLogger } from 'nestjs-pino';
 
 @Controller('log')
 export class LogController {
   private logger = new Logger(LogController.name);
 
-  constructor(private readonly logService: LogService) {}
+  constructor(
+    private readonly logService: LogService,
+    private readonly pinoLogger: PinoLogger,
+  ) {}
 
-  @Get()
+  @Get('/')
   testDefaultLogger() {
     this.logger.log('请求logger成功');
     return this.logService.hello();
+  }
+
+  @Get('/pino')
+  testPinoLogger() {
+    this.pinoLogger.info('请求nestjs-pino logger成功'); // 注释掉pino-log也会自动打印 懒人必备
+    return '请求nestjs-pino logger成功';
   }
 }
--- src/log/log.module.ts
+++ src/log/log.module.ts
@@ -1,8 +1,43 @@
 import { Module } from '@nestjs/common';
 import { LogService } from './log.service';
 import { LogController } from './log.controller';
+import { LoggerModule } from 'nestjs-pino';
+import { join } from 'node:path';
 
 @Module({
+  imports: [
+    /**
+     * 这部分学习 nestjs-pino 模块
+     * https://github.com/iamolegga/nestjs-pino
+     */
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          targets: [
+            process.env.NODE_ENV === 'development' // 本地测试的时候可以把这里打开
+              ? {
+                  level: 'info',
+                  target: 'pino-pretty',
+                  options: {
+                    colorize: true, // 格式化 更好看一些
+                  },
+                }
+              : {
+                  level: 'info',
+                  target: 'pino-roll',
+                  // 下面这个配置是生产环境启用日志存储的相关的配置
+                  options: {
+                    file: join('logs', 'pino-log.txt'),
+                    frequency: 'daily', // 打印周期 hourly
+                    size: '10m', // 单个文件大小， 如果超出了就新建文件
+                    mkdir: true,
+                  },
+                },
+          ],
+        },
+      },
+    }),
+  ],
   providers: [LogService],
   controllers: [LogController],
 })
